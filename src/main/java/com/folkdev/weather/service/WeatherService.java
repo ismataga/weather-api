3da19d4c14fb04ec8f47a4c9ce5e066a78086083
Ismat Aghamirzayev <ismataga1@gmail.com>
2023-06-26 06:11:45 +0400

    add some new properties WeatherService

--- src/main/java/com/folkdev/weather/service/WeatherService.java
+++ src/main/java/com/folkdev/weather/service/WeatherService.java
@@ -8,7 +8,15 @@ import com.folkdev.weather.dto.WeatherResponse;
 import com.folkdev.weather.model.WeatherEntity;
 import com.folkdev.weather.repository.WeatherRepository;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,8 +26,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Service
+@CacheConfig(cacheNames = {"weathers"})
 
 public class WeatherService {
+    private static final  Logger logger = LoggerFactory.getLogger(WeatherService.class);
     private final WeatherRepository weatherRepository;
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -30,7 +40,10 @@ public class WeatherService {
         this.restTemplate = restTemplate;
     }
 
+    @Cacheable(key = "#city")
     public WeatherDto getWeatherByCityName(String city) {
+
+        logger.info("requested city: "+city);
         Optional<WeatherEntity> weatherEntityOptional = weatherRepository.findFirstByRequestedCityNameOrderByUpdateTimeDesc(city);
         return weatherEntityOptional.map(weather -> {
                     if (weather.getUpdateTime().isBefore(LocalDateTime.now().minusMinutes(30))) {
@@ -52,12 +65,19 @@ public class WeatherService {
         }
     }
 
+    @CacheEvict(allEntries = true)
+    @PostConstruct
+    @Scheduled(fixedRateString = "10000")
+    public void clearCache() {
+        logger.info("cache cleared");
+    }
+
     private String getWeatherStackUrl(String city) {
-        return Constants.API_URL + Constants.ACCESS_KEY_PARAM + Constants.API_KEY + Constants.QUERY_KEY_PARAM+city;
+        return Constants.API_URL + Constants.ACCESS_KEY_PARAM + Constants.API_KEY + Constants.QUERY_KEY_PARAM + city;
     }
 
     private WeatherEntity saveWeatherEntity(String city, WeatherResponse weatherResponse) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:dd");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
         WeatherEntity weatherEntity = new WeatherEntity(city,
                 weatherResponse.location().name(),
